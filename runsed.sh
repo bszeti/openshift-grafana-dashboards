80c40d958fcd69c0fb89423656843b6b0f98b104
bszeti <bszeti@redhat.com>
2019-09-19 15:29:27 -0400

    no datasource

--- runsed.sh
+++ runsed.sh
@@ -1,4 +1,7 @@
-#sed 's/${DS_PROMETHEUS}/prometheus/g' summary-by-node.json.j2 | sudo sh -c $'sed \'s/{% filter_by_cluster %}/cluster=\\\\"$Cluster\\\\"/g\' >summary-by-node.json'
-sed 's/${DS_PROMETHEUS}/prometheus/g' summary-by-node.json.j2 | sed 's/{% filter_by_cluster %}/cluster=\\"$Cluster\\"/g' >summary-by-node.json
-#sed 's/${DS_PROMETHEUS}/prometheus/g' summary-by-nodetype.json.j2 | sudo sh -c $'sed \'s/{% filter_by_cluster %}/cluster=\\\\"$Cluster\\\\"/g\' summary-by-nodetype.json.j2 >summary-by-nodetype.json'
-sed 's/${DS_PROMETHEUS}/prometheus/g' summary-by-nodetype.json.j2 | sed 's/{% filter_by_cluster %}/cluster=\\"$Cluster\\"/g' >summary-by-nodetype.json
+# Aggregated dashboards - with cluster selection
+sed 's/${DS_PROMETHEUS}/prometheus/g' summary-by-node.json.j2 | sed 's/{% filter_by_cluster %}/cluster=\\"$Cluster\\"/g' >summary-by-node-aggregated.json
+sed 's/${DS_PROMETHEUS}/prometheus/g' summary-by-nodetype.json.j2 | sed 's/{% filter_by_cluster %}/cluster=\\"$Cluster\\"/g' >summary-by-nodetype-aggregated.json
+
+# Cluster dasboards - no cluster selection
+sed 's/${DS_PROMETHEUS}/prometheus/g' summary-by-node.json.j2 | sed 's/{% filter_by_cluster %}//g' >summary-by-node.json
+sed 's/${DS_PROMETHEUS}/prometheus/g' summary-by-nodetype.json.j2 | sed 's/{% filter_by_cluster %}//g' >summary-by-nodetype.json
