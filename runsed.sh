07ee8bcf1feeabfe312c56568d7c5b0c687f677b
bszeti <bszeti@redhat.com>
2019-09-19 12:37:54 -0400

    template summary-by-nodetype.json

--- runsed.sh
+++ runsed.sh
@@ -1,2 +1,2 @@
 sudo sh -c $'sed \'s/{% filter_by_cluster %}/cluster=\\\\"$Cluster\\\\"/g\' summary-by-node.json.j2 >summary-by-node.json'
-sudo sh -c $'sed \'s/{% filter_by_cluster %}/cluster=\\\\"$Cluster\\\\"/g\' summary-by-nodetype.json.2 >summary-by-node.json'
+sudo sh -c $'sed \'s/{% filter_by_cluster %}/cluster=\\\\"$Cluster\\\\"/g\' summary-by-nodetype.json.j2 >summary-by-node.json'
