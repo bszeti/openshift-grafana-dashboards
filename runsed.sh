9f94be60c877f07152f4ae52e7345a44ffaedb17
bszeti <bszeti@redhat.com>
2019-09-19 12:46:52 -0400

    Sed datasource

--- runsed.sh
+++ runsed.sh
@@ -1,2 +1,2 @@
-sudo sh -c $'sed \'s/{% filter_by_cluster %}/cluster=\\\\"$Cluster\\\\"/g\' summary-by-node.json.j2 >summary-by-node.json'
-sudo sh -c $'sed \'s/{% filter_by_cluster %}/cluster=\\\\"$Cluster\\\\"/g\' summary-by-nodetype.json.j2 >summary-by-node.json'
+sed 's/${DS_PROMETHEUS}/prometheus/g' summary-by-node.json.j2 | sudo sh -c $'sed \'s/{% filter_by_cluster %}/cluster=\\\\"$Cluster\\\\"/g\' >summary-by-node.json'
+sed 's/${DS_PROMETHEUS}/prometheus/g' summary-by-nodetype.json.j2 | sudo sh -c $'sed \'s/{% filter_by_cluster %}/cluster=\\\\"$Cluster\\\\"/g\' summary-by-nodetype.json.j2 >summary-by-nodetype.json'
