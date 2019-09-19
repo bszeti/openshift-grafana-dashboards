bd3bf8c64b83b944df4664897cc94a52141d3d62
bszeti <bszeti@redhat.com>
2019-09-19 13:46:39 -0400

    fix

--- runsed.sh
+++ runsed.sh
@@ -1,2 +1,4 @@
-sed 's/${DS_PROMETHEUS}/prometheus/g' summary-by-node.json.j2 | sudo sh -c $'sed \'s/{% filter_by_cluster %}/cluster=\\\\"$Cluster\\\\"/g\' >summary-by-node.json'
-sed 's/${DS_PROMETHEUS}/prometheus/g' summary-by-nodetype.json.j2 | sudo sh -c $'sed \'s/{% filter_by_cluster %}/cluster=\\\\"$Cluster\\\\"/g\' summary-by-nodetype.json.j2 >summary-by-nodetype.json'
+#sed 's/${DS_PROMETHEUS}/prometheus/g' summary-by-node.json.j2 | sudo sh -c $'sed \'s/{% filter_by_cluster %}/cluster=\\\\"$Cluster\\\\"/g\' >summary-by-node.json'
+sed 's/${DS_PROMETHEUS}/prometheus/g' summary-by-node.json.j2 | sed 's/{% filter_by_cluster %}/cluster=\\"$Cluster\\"/g' >summary-by-node.json
+#sed 's/${DS_PROMETHEUS}/prometheus/g' summary-by-nodetype.json.j2 | sudo sh -c $'sed \'s/{% filter_by_cluster %}/cluster=\\\\"$Cluster\\\\"/g\' summary-by-nodetype.json.j2 >summary-by-nodetype.json'
+sed 's/${DS_PROMETHEUS}/prometheus/g' summary-by-nodetype.json.j2 | sed 's/{% filter_by_cluster %}/cluster=\\"$Cluster\\"/g' >summary-by-nodetype.json
